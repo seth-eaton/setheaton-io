d9e6b464b225c0124c0ce75b9923421caebb7a35
Seth Eaton <ste2108@columbia.edu>
2023-02-01 17:19:03 -0500

    updated homepage and added 404

--- mysite/homepage/views.py
+++ mysite/homepage/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
 
 # Create your views here.
 def home(request):
     return render(request, 'homepage/home.html')
+
+def not_found_view(request, exception):
+    if request.path.count('sethtunes/') > 0:
+        return render(request, 'homepage/sethtunes_404.html')
+    else:
+        return render(request, 'homepage/404.html')
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,3 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homepage.urls')),
 ]
+
+handler404 = 'homepage.views.not_found_view'
