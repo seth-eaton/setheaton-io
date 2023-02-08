8a4bfa7fe2c77008650f742cf5f485975a65f777
Seth Eaton <ste2108@columbia.edu>
2023-02-08 18:53:25 -0500

    updated wiki scrape algo

--- mysite/sethtunes/management/commands/initialwikiscrape.py
+++ mysite/sethtunes/management/commands/initialwikiscrape.py
@@ -11,14 +11,20 @@ class Command(BaseCommand):
         wiki = wikipedia.Wikipedia('en')
 
         for artist in Artist.objects.all():
-            page = wiki.page(artist.artist_name)
-            add = False
-            if page.exists():
-                for category in page.categories:
-                    if 'singer' in category.lower() or 'musician' in category.lower() or 'music group' in category.lower() or 'musical group' in category.lower():
-                        add = True
-            if add:
-                artist.wikiblurb_set.create(url=page.fullurl, summary=page.summary)
-                self.stdout.write(self.style.SUCCESS('Found wiki for %s' % artist.artist_name))
+            if artist.wikiblurb_set.count() == 0:
+                try:
+                    page = wiki.page(artist.artist_name)
+                    add = False
+                    if page.exists():
+                        for category in page.categories:
+                            if 'singer' in category.lower() or 'musician' in category.lower() or 'music group' in category.lower() or 'musical group' in category.lower():
+                                add = True
+                    if add:
+                        artist.wikiblurb_set.create(url=page.fullurl, summary=page.summary)
+                        self.stdout.write(self.style.SUCCESS('Found wiki for %s' % artist.artist_name))
+                    else:
+                        self.stdout.write(self.style.NOTICE('Could not find wiki for %s' % artist.artist_name))
+                except:
+                    self.stdout.write(self.style.NOTICE('Could not find wiki for %s' % artist.artist_name))
             else:
-                self.stdout.write(self.style.NOTICE('Could not find wiki for %s' % artist.artist_name))
+                self.stdout.write(self.style.NOTICE('There is already a wiki for %s' % artist.artist_name))
