faf03180783757b0710dfc204f0a492dc0469e5f
Seth Eaton <ste2108@columbia.edu>
2023-02-13 14:20:06 -0500

    code to search pf for latest reviews

--- mysite/sethtunes/tasks.py
+++ mysite/sethtunes/tasks.py
@@ -3,12 +3,39 @@ from celery import shared_task
 from datetime import datetime
 
 from django.utils import timezone
-from sethtunes.models import Artist, Album, Song, Embed
+from sethtunes.models import Artist, Album, Song, Embed, PFReview
 from urllib.parse import urljoin, quote
+from urllib.request import urlopen
+from urllib.request import Request
+from bs4 import BeautifulSoup
 from decouple import config
 import applemusicpy
 import datetime
 import json
+import re
+
+@shared_task(name = "get_latest_pf_sethtunes")
+def get_latest():
+    
+    request = Request(url='https://pitchfork.com/reviews/albums/',
+                      data=None,
+                      headers={'User-Agent': 'tegassharma/pitchfork-v0.1'})
+    response = urlopen(request)
+    text = response.read().decode('UTF-8').split('window.App=')[1].split(';</script>')[0]
+
+    obj = json.loads(text)
+    reviews = obj['context']['dispatcher']['stores']['ReviewsStore']['items']
+
+    for review in reviews:
+        review_artist = reviews[review]['artists'][0]['display_name']
+        review_album = reviews[review]['tombstone']['albums'][0]['album']['display_name']
+        
+        try:
+            album = Album.objects.filter(artist_name=review_artist).filter(album_name=review_album).get()
+            if album.pfreview_set.count() == 0:
+                add_review(album, reviews[review]['url'])
+        except:
+            pass
 
 @shared_task(name = "update_artist_main")
 def update_artist():
@@ -116,3 +143,86 @@ def release_date(date_str):
     date =  datetime.datetime.strptime(date_str, '%Y-%m-%d')
     date_aware = date.replace(tzinfo=datetime.timezone.utc)
     return date_aware
+
+def add_review(album, url):
+    full_url = urljoin('http://pitchfork.com/', url)
+    request = Request(url=full_url,
+                      data=None,
+                      headers={'User-Agent': 'tejassharma/pitchfork-v0.1'})
+    response_text = urlopen(request).read()
+    soup = BeautifulSoup(response_text, "lxml")
+
+    if soup.find(class_='review-multi') is None:
+        try:
+            r = PFReview(album=album, album_name=album.album_name, artist_name=album.artist_name, url=url, score=score(soup), author=author(soup), abstract=abstract(soup), editorial=editorial(soup), bnm=bnm(soup))
+            r.save()
+            print('Added PF review for %s' % album.album_name)
+        except:
+            print('Could not add PF review for %s' % album.album_name)
+    else:
+        print('Found multi review for %s' % album.album_name)
+
+def find_pf(self, artist_name, album_name, album):
+    query = '{} {}'.format(artist_name, album_name)
+    query = quote(query)
+    # using a custom user agent header
+    request = Request(url='http://pitchfork.com/search/?query=' + query,
+                      data=None,
+                      headers={'User-Agent': 'tejassharma/pitchfork-v0.1'})
+    response = urlopen(request)
+    text = response.read().decode('UTF-8').split('window.App=')[1].split(';</script>')[0]
+
+    # the server responds with json so we load it into a dictionary
+    obj = json.loads(text)
+
+    try:
+        # get the nested dictionary containing url to the review and album name
+        review_dict = obj['context']['dispatcher']['stores']['SearchStore']['results']['albumreviews']['items'][0]
+
+        url = review_dict['url']
+        matched_artist = review_dict['artists'][0]['display_name']
+
+        #print(json.dumps(review_dict, indent=2))
+
+        # fetch the review page
+        full_url = urljoin('http://pitchfork.com/', url)
+        request = Request(url=full_url,
+                          data=None,
+                          headers={'User-Agent': 'tejassharma/pitchfork-v0.1'})
+        response_text = urlopen(request).read()
+        soup = BeautifulSoup(response_text, "lxml")
+
+        if soup.find(class_='review-multi') is None:
+            r = PFReview(album=album, album_name=album_name, artist_name=artist_name, url=url, score=self.score(soup), author=self.author(soup), abstract=self.abstract(soup), editorial=self.editorial(soup), bnm=self.bnm(soup))
+            r.save()
+            self.stdout.write(self.style.SUCCESS('Found PF review for %s' % album_name))
+        else:
+            pass
+    except IndexError:
+        pass
+
+def score(soup):
+    rating = soup.find('p', attrs={'class': re.compile('.*Rating.*')}).text
+    return rating.strip()
+
+def author(soup):
+    return soup.find('a', attrs={'class': re.compile('.*byline__name-link.*')}).get_text()
+
+def abstract(soup):
+    return soup.find('div', attrs={'class': re.compile('.*SplitScreenContentHeaderDekDown.*')}).get_text()
+
+def editorial(soup):
+    editorials = soup.find_all(class_='body__inner-container')
+    text = ''
+    for editorial in editorials:
+        for el in editorial:
+            if el.name == 'p':
+                text += el.text + '\n\n'
+            elif el.name == 'hr':
+                return text
+            else:
+                pass
+    return text
+
+def bnm(soup):
+    return soup.find('p', attrs={'class': re.compile('.*BestNewMusic.*')}) != None
